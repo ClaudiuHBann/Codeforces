14816f745570e0462f6c809a1c30efb8b01bedd2
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2021-07-21 07:25:49 -0700

    Create 263A - Beautiful Matrix.cpp

---
+++ 263A - Beautiful Matrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+	unsigned char line, column;
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			unsigned char element;
+			std::cin >> element;
+
+			if (element - '0' == 1)
+			{
+				line = i + 1;
+				column = j + 1;
+			}
+		}
+	}
+
+	std::cout << abs(line - 3) + abs(column - 3);
+
+	return 0;
+}
