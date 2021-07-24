7eaa5e34ad71f4f6672ef482a7ee78c23cb339de
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2021-07-24 14:37:43 -0700

    Create 58A - Chat Room.cpp

---
+++ 58A - Chat Room.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string word, hello("hello");
+	std::cin >> word;
+
+	unsigned char helloIterator = 0;
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (word[i] == hello[helloIterator])
+		{
+			helloIterator++;
+
+			if (helloIterator == hello.length())
+			{
+				break;
+			}
+		}
+	}
+
+	std::cout << ((helloIterator == hello.length()) ? "YES" : "NO");
+}
