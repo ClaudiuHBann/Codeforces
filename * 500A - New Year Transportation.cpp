1ee278a5c9e2ce321307d810143b26c086dbd901
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2021-07-26 08:49:14 -0700

    Update * 500A - New Year Transportation.cpp

--- * 500A - New Year Transportation.cpp
+++ * 500A - New Year Transportation.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
 
+int main()
+{
+	unsigned short numberOfCells, destinationCellIndex;
+	std::cin >> numberOfCells >> destinationCellIndex;
+
+	std::vector<unsigned short> portals;
+	for (int i = 0; i < numberOfCells - 1; i++)
+	{
+		unsigned short nextPortalIndex;
+		std::cin >> nextPortalIndex;
+
+		portals.push_back(nextPortalIndex);
+	}
+
+	unsigned short currentIndex = 1;
+	for (int i = 0; i < portals.size(); i++)
+	{
+		if (currentIndex == destinationCellIndex)
+		{
+			break;
+		}
+
+		currentIndex = i + portals[i] + 1;
+	}
+
+	std::cout << ((currentIndex == destinationCellIndex) ? "YES" : "NO");
+
+	return 0;
+}
