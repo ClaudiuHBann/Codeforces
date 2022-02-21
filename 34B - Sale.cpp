4482abdd723ea4792058ed86a29744bf13179343
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2022-02-21 00:52:24 -0800

    Update 34B - Sale.cpp

--- 34B - Sale.cpp
+++ 34B - Sale.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <math.h>
+
+#define ULL unsigned long long
+#define LL long long
+
+#define FAST_IO() std::ios_base::sync_with_stdio(false), std::cin.tie(nullptr);
 
 int main() {
+	FAST_IO();
+
 	int n, m;
 	std::cin >> n >> m;
 	std::vector<int> tvs(n);
 	for(size_t i = 0; i < n; i++)
 		std::cin >> tvs[i];
 
-	std::vector<int> v(m);
-	for(size_t i = 0; i < m; i++)
-		v[i] = tvs[i];
-
-	for(size_t i = m; i < n; i++) {
-		int indexOfBiggest = 0;
-		for(size_t j = 1; j < m; j++)
-			if(v[indexOfBiggest] < v[j])
-				indexOfBiggest = j;
-
-		if(tvs[i] < v[indexOfBiggest])
-			v[indexOfBiggest] = tvs[i];
-	}
+	std::sort(tvs.begin(), tvs.end());
 
 	int sum = 0;
 	for(size_t i = 0; i < m; i++)
-		if(sum + v[i] < sum)
-			sum += v[i];
+		if(sum + tvs[i] < sum)
+			sum += tvs[i];
+		else
+			break;
 
 	std::cout << abs(sum);
 
