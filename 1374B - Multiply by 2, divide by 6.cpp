9f3c0d8466c71825831f4492fc35e28b75e7d4c8
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2022-02-21 01:08:37 -0800

    Update 1374B - Multiply by 2, divide by 6.cpp

--- 1374B - Multiply by 2, divide by 6.cpp
+++ 1374B - Multiply by 2, divide by 6.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 
+#define ULL unsigned long long
+#define LL long long
+
+#define FAST_IO() std::ios_base::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+
 int main() {
+	FAST_IO();
+
 	int t;
 	std::cin >> t;
 
-	for(size_t i = 0; i < t; i++) {
-		long long n;
+	while(t--) {
+		LL n;
 		std::cin >> n;
 
 		int moves = 0;
-		while(n != 1) {
-			moves++;
-
-			n = (n % 6 == 0) ? n / 6 : n * 2;
-
-			if(n > INT32_MAX) {
+		while(n != 1)
+			if(n % 6 == 0) {
+				n /= 6;
+				moves++;
+			} else if(n % 3 == 0) {
+				n *= 2;
+				n /= 6;
+				moves += 2;
+			} else {
 				moves = -1;
 				break;
 			}
-		}
 
 		std::cout << moves << std::endl;
 	}
