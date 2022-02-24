ba5445127e8daf307c4d8630a4b441d6c6592eb3
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2022-02-24 09:27:36 -0800

    Create 1374C - Move Brackets.cpp

---
+++ 1374C - Move Brackets.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define pb push_back
+
+#define vi vector<int>
+
+#define co(condition, left, right) ((condition) ? left : right)
+
+#define bug(object) cout << endl << "bug(" << #object << " = " << object << ")" << endl;
+
+#define FAST_IO() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+int main() {
+	FAST_IO();
+
+	int t;
+	cin >> t;
+
+	while(t--) {
+		int n;
+		string brackets;
+		cin >> n >> brackets;
+
+		int openedBrackets = 0, wrongBrackets = 0;
+		bool between = false;
+		for(size_t i = 0; i < n; i++)
+			if(brackets[i] == '(')
+				if(between)
+					openedBrackets++;
+				else
+					between = true;
+			else
+				if(between)
+					if(openedBrackets > 0)
+						openedBrackets--;
+					else {
+						between = false;
+						openedBrackets = 0;
+					} else
+						wrongBrackets++;
+
+		cout << wrongBrackets << endl;
+	}
+
+	return 0;
+}
