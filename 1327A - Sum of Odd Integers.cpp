ce9fd586543aaf1610a98b62b238dfe40543afe3
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2022-03-05 06:37:01 -0800

    Create 1327A - Sum of Odd Integers.cpp

---
+++ 1327A - Sum of Odd Integers.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+#define vi vector<int>
+#define mii map<int, int>
+#define pii pair<int, int>
+
+#define co(condition, left, right) ((condition) ? (left) : (right))
+#define NEXT(type) NextT<type>()
+
+#define BUG(object) cout << endl << "\x1B[31mBUG(" << #object << " = " << object << ")\x1B[0m" << endl;
+#define FAST_IO() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+template<typename T>
+T NextT() {
+	T nextT;
+	cin >> nextT;
+	return nextT;
+}
+
+int main() {
+	FAST_IO();
+
+	int t = NEXT(int);
+	while(t--) {
+		int n = NEXT(int), k = NEXT(int);
+
+		cout << co((n % 2 == 0 && k % 2) || (n % 2 && k % 2 == 0) || pow(k, 2) > n, "NO", "YES") << endl;
+	}
+
+	return 0;
+}
